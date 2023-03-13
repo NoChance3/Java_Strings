dbf07ff0e4e1ecea4d4dadeee8f7a4ad70d5ee39
viacheslav_derevianko <malorka0540205205@mail.ru>
2023-03-13 22:13:57 +0300

    done х1

--- src/main/java/practice/string/SequentialWordsNumbers.java
+++ src/main/java/practice/string/SequentialWordsNumbers.java
@@ -1,8 +1,44 @@
 package practice.string;
 
+import java.util.Scanner;
+
+
 public class SequentialWordsNumbers {
 
-    public static String sequentialWordsNumbers(String text){
-        return "";
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        System.out.println(sequentialWordsNumbers(text));
+    }
+
+
+
+
+
+
+    public static String sequentialWordsNumbers(String text) {
+        int start = 0;
+        int end;
+        int num = 1;
+        String endText = "(1) ";
+
+        for (int i = 0; i < text.length(); i++) {
+            end = i;
+            if (text.charAt(i) == ' ') {
+                while (start < end) {
+                    endText = endText.concat(String.valueOf(text.charAt(start)));
+                    start++;
+                }
+                num++;
+                endText = endText.concat(" (" + num + ")");
+            }
+            while (start <= end) {
+                endText = endText.concat(String.valueOf(text.charAt(start)));
+                start++;
+            }
+        }
+        return endText;
+
     }
 }
