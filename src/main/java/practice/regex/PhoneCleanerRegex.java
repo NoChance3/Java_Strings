761286bb83fbc0735fcc2eab1e1ae3093b47a057
Vyasheslav Derevyanko <malorka0540205205@mail.ru>
2023-03-16 21:50:45 +0300

    done x3

--- src/main/java/practice/regex/PhoneCleanerRegex.java
+++ src/main/java/practice/regex/PhoneCleanerRegex.java
@@ -4,17 +4,43 @@ import java.util.Scanner;
 
 public class PhoneCleanerRegex {
 
+  public static String number(String input) {
+    input = input.replaceAll("[^0-9]","");
+    if (input.matches("7[0-9]{10}")){
+      return input;
+    }
+    else if (input.matches("8[0-9]{10}")) {
+      input = input.replaceFirst("8", "7");
+      return input;
+    }
+    else if (input.matches("9[0-9]{9}")) {
+      input = input.replaceFirst("9", "79");
+      return input;
+    }
+    else if (input.matches("[0-9]{12,}")){
+      return "Неверный формат номера";
+    }
+    else if(input.matches("[^7-8][0-9]{10}")){
+      return "Неверный формат номера";
+    }
+    else if(input.matches("[0-9]{2,10}")){
+      return "Неверный формат номера";
+    }
+    else return "";
+  }
+
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
     while (true) {
       String input = scanner.nextLine();
+      System.out.println(number(input));
       if (input.equals("0")) {
         scanner.close();
         break;
       }
 
-      // TODO:напишите ваш код тут, результат вывести в консоль.
+
     }
   }
 
